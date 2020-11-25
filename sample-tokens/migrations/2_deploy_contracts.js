6a067a053727cb193ce696b8ad7c993e118be013
John Paul Cas <casjp.app@gmail.com>
2020-11-25 20:39:36 +0800

    add sample token supply

--- sample-tokens/migrations/2_deploy_contracts.js
+++ sample-tokens/migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 const Token1 = artifacts.require("MyToken1.sol");
 const Token2 = artifacts.require("MyToken2.sol");
 
-module.exports = function (deployer) {
-    await deployer.deploy(Token1);
-    await deployer.deploy(Token2);
+module.exports = async function (deployer) {
+    await deployer.deploy(Token1, web3.utils.toWei('1000000', 'ether'));
+    await deployer.deploy(Token2, web3.utils.toWei('1000000', 'ether'));
 };
