87589811e2f5a79ebfa607de1eaada170973d2f3
John Paul Cas <casjp.app@gmail.com>
2020-11-25 21:02:11 +0800

    comment unused methods

--- periphery/truffle-config.js
+++ periphery/truffle-config.js
@@ -43,9 +43,10 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      gas: 8205585,
     },
     // Another network with more advanced options...
     // advanced: {
